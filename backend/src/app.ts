6c646c59121190246d384d9e908b70ec1b680a68
Haris <harisgujjar317@gmail.com>
2024-04-15 18:13:28 -0400

    changes made for deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json())
-app.use(cors( { origin: 'http://localhost:5173', credentials: true  }));
+app.use(cors( { origin: 'https://ai-chat-app-wmwf.vercel.app', credentials: true  }));
 
 //will not work in production
 app.use(morgan("dev"));
