e1001cbcfd0be1d7baafa227a7d2e3ddbc4a96a4
Haris <harisgujjar317@gmail.com>
2024-04-16 12:53:27 -0400

    testing

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -10,9 +10,9 @@ export const createToken = (id: string, email: string, expiresIn: string) => {
 
 export const verifyToken = async(req: Request, res: Response, next: NextFunction) => {
     const token = await req.signedCookies[`${COOKIE_NAME}`];
-    // if (!token || token.trim() === "") {
-    //     return res.status(401).json({ message: "Token Not Received"});
-    // }
+    if (!token || token.trim() === "") {
+        return res.status(401).json({ message: "Token Not Received"});
+    }
     return new Promise<void>((resolve, reject) => {
         return jwt.verify(token, process.env.JWT_SECRET, (error, success) => {
             if (error) {
